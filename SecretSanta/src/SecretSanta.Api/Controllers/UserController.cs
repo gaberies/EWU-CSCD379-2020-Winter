73eba1a57af8b2861f46f6c7f43a17e42212a4cc
Hunter Matthew Rich <xxrichmanxx@gmail.com>
2020-02-03 11:42:36 -0800

    Startup code

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IAuthorService AuthorService { get; }
+
+        public AuthorController(IAuthorService authorService)
+        {
+            AuthorService = authorService ?? throw new System.ArgumentNullException(nameof(authorService));
+        }
+
+        // GET: https://localhost/api/Author
+        [HttpGet]
+        public async Task<IEnumerable<Author>> Get()
+        {
+            List<Author> authors = await AuthorService.FetchAllAsync();
+            return authors;
+        }
+
+        // GET: api/Author/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Author>> Get(int id)
+        {
+            if (await AuthorService.FetchByIdAsync(id) is Author author)
+            {
+                return Ok(author);
+            }
+            return NotFound();
+        }
+
+        // POST: api/Author
+        [HttpPost]
+        public async Task<Author> Post(Author value)
+        {
+            return await AuthorService.InsertAsync(value);
+        }
+
+        // PUT: api/Author/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Author>> Put(int id, Author value)
+        {
+            if (await AuthorService.UpdateAsync(id, value) is Author author)
+            {
+                return author;
+            }
+            return NotFound();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -1,7 +1,14 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SecretSanta.Business;
+using SecretSanta.Business.Services;
+using SecretSanta.Data;
 
 namespace SecretSanta.Api
 {
@@ -17,7 +24,30 @@ namespace SecretSanta.Api
         //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public static void Configure(IServiceCollection services)
+        {
+            var sqliteConnection = new SqliteConnection("DataSource=:memory:");
+            sqliteConnection.Open();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.EnableSensitiveDataLogging()
+                       .UseSqlite(sqliteConnection));
+
+            services.AddScoped<IGiftService, GiftService>();
+            services.AddScoped<IUserService, UserService>();
+
+            System.Type profileType = typeof(AutomapperConfigurationProfile);
+            System.Reflection.Assembly assembly = profileType.Assembly;
+            services.AddAutoMapper(new[] { assembly });
+
+            services.AddMvc(opts => opts.EnableEndpointRouting = false);
+
+            services.AddSwaggerDocument();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -26,13 +56,11 @@ namespace SecretSanta.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                _ = endpoints.MapGet("/", async context =>
-                  {
-                      await context.Response.WriteAsync("Hello from API!");
-                  });
-            });
+            app.UseOpenApi();
+            //http://localhost/swagger
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
         }
     }
 }
