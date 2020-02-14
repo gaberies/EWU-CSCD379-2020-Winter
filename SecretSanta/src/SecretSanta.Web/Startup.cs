e5f9940688a974468c8f12b13073d51e9a219811
Hunter Matthew Rich <xxrichmanxx@gmail.com>
2020-02-13 16:31:00 -0800

    Bullet 1,2,3-6 Done

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -9,17 +9,26 @@ namespace SecretSanta.Web
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public static void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
-
-            services.AddHttpClient("SecretSantaApi");
+            services.AddControllersWithViews();
+            services.AddHttpClient("SecretSantaApi", options =>
+            {
+                options.BaseAddress = new Uri(Configuration["ApiUrl"]);
+            });
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Warning generated in class.")]
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -27,10 +36,15 @@ namespace SecretSanta.Web
             }
 
             app.UseHttpsRedirection();
+            app.UseRouting();
 
+            // Do we want to use static files here?
             app.UseStaticFiles();
 
-            app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapDefaultControllerRoute();
+            });
         }
     }
 }
